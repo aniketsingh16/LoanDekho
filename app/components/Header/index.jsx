6f7c48cb9e54c93e226d089b6090cd026fc78282
Singh <aniket.b.singh@capgemini.com>
2024-11-22 01:14:53 +0530

    Initial Commit

---
+++ app/components/Header/index.jsx
@@ -0,0 +1,87 @@
+"use client";
+import { useState } from "react";
+import { Row, Col, Drawer } from "antd";
+import Container from "../../common/Container";
+import { SvgIcon } from "../../common/SvgIcon";
+import { Button } from "../../common/Button";
+import {
+  HeaderSection,
+  LogoContainer,
+  Burger,
+  NotHidden,
+  Menu,
+  CustomNavLinkSmall,
+  Label,
+  Outline,
+  Span,
+} from "./styles";
+
+const Header = () => {
+  const [visible, setVisibility] = useState(false);
+
+  const toggleButton = () => {
+    setVisibility(!visible);
+  };
+
+  const MenuItem = () => {
+    const scrollTo = (id) => {
+      const element = document.getElementById(id);
+      element.scrollIntoView({
+        behavior: "smooth",
+      });
+      setVisibility(false);
+    };
+
+    return (
+      <>
+        <CustomNavLinkSmall onClick={() => scrollTo("mission")}>
+          <Span>Home Loan</Span>
+        </CustomNavLinkSmall>
+        <CustomNavLinkSmall onClick={() => scrollTo("product")}>
+          <Span>Car Loan</Span>
+        </CustomNavLinkSmall>
+        <CustomNavLinkSmall onClick={() => scrollTo("mission")}>
+          <Span>Personal Loan</Span>
+        </CustomNavLinkSmall>
+        <CustomNavLinkSmall
+          style={{ width: "180px" }}
+          onClick={() => scrollTo("contact")}
+        >
+          <Span>
+            <Button>Contact Us</Button>
+          </Span>
+        </CustomNavLinkSmall>
+      </>
+    );
+  };
+
+  return (
+    <HeaderSection>
+      <Container>
+        <Row justify="end">
+          <NotHidden>
+            <MenuItem />
+          </NotHidden>
+          <Burger onClick={toggleButton}>
+            <Outline />
+          </Burger>
+        </Row>
+        <Drawer closable={false} open={visible} onClose={toggleButton}>
+          <Col style={{ marginBottom: "2.5rem" }}>
+            <Label onClick={toggleButton}>
+              <Col span={12}>
+                <Menu>Menu</Menu>
+              </Col>
+              <Col span={12}>
+                <Outline />
+              </Col>
+            </Label>
+          </Col>
+          <MenuItem />
+        </Drawer>
+      </Container>
+    </HeaderSection>
+  );
+};
+
+export default Header;
